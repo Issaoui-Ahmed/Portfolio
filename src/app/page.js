42a5081a29c6ca32628ecbefca8f71ee29e53b03
Issaoui-Ahmed <aissaoui2022@gmail.com>
2026-01-08 12:30:06 -0500

    Initial commit from Create Next App

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div style={{ padding: "50px", textAlign: "center" }}>
+            <h1>Portfolio V1</h1>
+            <p>Welcome to my portfolio! The server is running correctly.</p>
+        </div>
+    );
+}
